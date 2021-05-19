7425c5cf9a7d8f11d088458b2c8412c097d0e580
Aldo Dell Uomini <aldodell@gmail.com>
2021-05-19 11:35:29 -0400

    desarrollo

---
+++ ChecklistCompleted/AircraftSelectorView.swift
@@ -0,0 +1,27 @@
+//
+//  AircraftSelectorView.swift
+//  ChecklistCompleted
+//
+//  Created by Aldo Dell Uomini on 17/5/21.
+//
+
+import SwiftUI
+
+struct AircraftSelectorView : View {
+    var body: some View {
+        
+        Text("x")
+            .padding()
+            .background(Color.yellow)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+           
+    }
+    
+}
+
+
+struct AircraftSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        AircraftSelectorView()
+    }
+}
